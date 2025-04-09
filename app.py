ff193e24a6193ed5587a197e78690f0c52a86808
yt02 <yeethong02@yahoo.com>
2025-04-09 23:17:58 +0800

    update app.py

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def analyze():
     return jsonify({'results': results})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
+
